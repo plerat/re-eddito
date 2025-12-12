76b82c6e9cd743879443edb85b78a17091d9db26
candaes cedric <118951399+candaesCedric@users.noreply.github.com>
2025-12-12 23:15:04 +0100

    add user profile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 
-use App\Entity\User;
+use App\Form\ProfileType;
+use App\Service\User\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -14,8 +15,30 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 final class UserController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function profile(): Response
+    public function profile(Request $request, UserService $userService): Response
     {
-        return $this->render('user/profile.html.twig',[]);
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileType::class);
+        $form->handleRequest($request);
+
+        $error = null;
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newPseudo = $form->get('pseudo')->getData();
+            $plainPassword = $form->get('_password')->getData();
+
+            if (!$userService->isValidPassword($user, $plainPassword)) {
+                $error = 'Mot de passe incorrect.';
+            } elseif ($userService->isPseudoTaken($newPseudo, $user)) {
+                $error = 'Ce pseudo est déjà utilisé.';
+            } else {
+                $userService->updatePseudo($user, $newPseudo);
+                return $this->redirectToRoute('app_profile');
+            }
+        }
+        return $this->render('user/profile.html.twig', [
+            'form' => $form,
+            'error' => $error,
+        ]);
     }
 }
