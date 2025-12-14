503e6df0e9796c34b4108a070726254d76ce06f2
candaes cedric <118951399+candaesCedric@users.noreply.github.com>
2025-12-14 14:33:32 +0100

    backtrack MediaVoter

--- src/Security/Voter/MediaVoter.php
+++ src/Security/Voter/MediaVoter.php
@@ -5,35 +5,54 @@ namespace App\Security\Voter;
 use App\Entity\Media;
 use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authorization\Voter\Vote;
 
-class MediaVoter extends Voter
+class MediaVoter
 {
-    public const DELETE_MEDIA = 'delete_media';
+    const string DELETE_MEDIA = 'delete_media';
 
     protected function supports(string $attribute, mixed $subject): bool
     {
-        return $attribute === self::DELETE_MEDIA && $subject instanceof Media;
+        if (!in_array($attribute, [self::DELETE_MEDIA])) {
+            return false;
+        }
+
+        if (!$subject instanceof Media) {
+            return false;
+        }
+        return true;
     }
 
-    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token, ?Vote $vote = null): bool
     {
         $user = $token->getUser();
         if (!$user instanceof User) {
+            // the user must be logged in; if not, deny access
+            $vote?->addReason('The user is not logged in.');
             return false;
         }
 
         $media = $subject;
+        return match ($attribute) {
+            self::DELETE_MEDIA => $this->canDelete($media, $user, $vote),
+            default => throw new \LogicException('This code should not be reached!')
+        };
+    }
 
-        // auteur du post
-        if ($user === $media->getPost()->getCreatedBy()) {
+    private function canDelete(Media $media, User $user, ?Vote $vote): bool {
+
+        if ($user ===  $media->getPost()->getCreatedBy()) {
             return true;
         }
 
-        // admin
-        if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+        if (in_array('ROLE_ADMIN', $user->getRoles())) {
             return true;
         }
+
+        $vote?->addReason(sprintf(
+            'The logged in user (email: %s) is neither the author of this media (id: %d) or Administrator.',
+            $user->getEmail(), $media->getId()
+        ));
 
         return false;
     }
